d710a9a43c71d6d251c92de251dec6ccd9a1b0c8
Jared Friese <jared.friese@gmail.com>
2016-07-31 09:26:33 -0700

    Create script to copy storyboard info files into test bundle
    [#127418889]

---
+++ Fleet/Script/copy_storyboard_info_files.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+COPY_DESTINATION_PATH="$TARGET_BUILD_DIR"/"$CONTENTS_FOLDER_PATH"/StoryboardInfo
+if [ -d "$COPY_DESTINATION_PATH" ]; then
+rm -rf $COPY_DESTINATION_PATH
+fi
+
+mkdir $COPY_DESTINATION_PATH
+
+# Copy all storyboard Info.plist files into the created directory
+TEST_HOST_APP_NAME=$(echo "$TEST_HOST" | rev | cut -d '/' -f1 | rev)
+TEST_HOST_INTERMEDIATE_BUILD_FOLDER="$CONFIGURATION_TEMP_DIR"/"$TEST_HOST_APP_NAME".build
+for storyboardFolder in "$TEST_HOST_INTERMEDIATE_BUILD_FOLDER"/*.storyboardc; do
+    STORYBOARD_NAME=$(echo "$storyboardFolder" | rev | cut -d '/' -f1 | rev | cut -d '.' -f1)
+    mkdir "$COPY_DESTINATION_PATH"/"$STORYBOARD_NAME"
+    cp "$TEST_HOST_INTERMEDIATE_BUILD_FOLDER"/"$STORYBOARD_NAME".storyboardc/Info.plist "$COPY_DESTINATION_PATH"/"$STORYBOARD_NAME"
+done
