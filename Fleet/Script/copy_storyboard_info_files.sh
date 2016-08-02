801a43a314a85ba1cc5b161dc0ec28bb2a5dd9be
Jared Friese <jared.friese@gmail.com>
2016-08-01 17:26:40 -0700

    Grab all recursive storyboard files for use in test

--- Fleet/Script/copy_storyboard_info_files.sh
+++ Fleet/Script/copy_storyboard_info_files.sh
@@ -10,8 +10,8 @@ mkdir $COPY_DESTINATION_PATH
 # Copy all storyboard Info.plist files into the created directory
 TEST_HOST_APP_NAME=$(echo "$TEST_HOST" | rev | cut -d '/' -f1 | rev)
 TEST_HOST_INTERMEDIATE_BUILD_FOLDER="$CONFIGURATION_TEMP_DIR"/"$TEST_HOST_APP_NAME".build
-for storyboardFolder in "$TEST_HOST_INTERMEDIATE_BUILD_FOLDER"/*.storyboardc; do
+for storyboardFolder in $(find "$TEST_HOST_INTERMEDIATE_BUILD_FOLDER" -name "*.storyboardc"); do
     STORYBOARD_NAME=$(echo "$storyboardFolder" | rev | cut -d '/' -f1 | rev | cut -d '.' -f1)
     mkdir "$COPY_DESTINATION_PATH"/"$STORYBOARD_NAME"
-    cp "$TEST_HOST_INTERMEDIATE_BUILD_FOLDER"/"$STORYBOARD_NAME".storyboardc/Info.plist "$COPY_DESTINATION_PATH"/"$STORYBOARD_NAME"
+    cp "$storyboardFolder"/Info.plist "$COPY_DESTINATION_PATH"/"$STORYBOARD_NAME"
 done
