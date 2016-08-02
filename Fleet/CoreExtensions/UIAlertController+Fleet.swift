a1456c6d68582c4041b4789c7174b665b13f7443
Jared Friese <jared.friese@gmail.com>
2016-08-01 20:39:07 -0700

    Add in-code documentation to UIAlertController+Fleet
    [#126593423]

--- Fleet/CoreExtensions/UIAlertController+Fleet.swift
+++ Fleet/CoreExtensions/UIAlertController+Fleet.swift
@@ -1,11 +1,17 @@
 import Foundation
 
 public extension UIAlertController {
+    /**
+        Mimics a tap on the alert item with the given title,
+        firing any associated behavior.
+
+        - Parameter title:  The title of the action to tap
+    */
     public func tapAlertActionWithTitle(title: String) {
         let filteredActions = actions.filter { action in
             return action.title == title
         }
-        
+
         if let actionWithTitle = filteredActions.first {
             if let handler = actionWithTitle.handler {
                 handler(actionWithTitle)
