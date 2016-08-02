9c0738eac21085ac144059b41fe6e87c86b9cd4c
Jared Friese <jared.friese@gmail.com>
2016-08-01 20:39:46 -0700

    Add in-code documentation to UIButton+Fleet
    [#126593423]

--- Fleet/CoreExtensions/UIButton+Fleet.swift
+++ Fleet/CoreExtensions/UIButton+Fleet.swift
@@ -1,6 +1,10 @@
 import Foundation
 
 public extension UIButton {
+
+    /**
+        Mimics a user tap on the button, firing any associated events
+    */
     public func tap() {
         sendActionsForControlEvents(.TouchUpInside)
     }
