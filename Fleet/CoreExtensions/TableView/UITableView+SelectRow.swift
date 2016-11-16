55da6017fa238953ca8dede52a7230ce8a48d44f
Jared Friese <jared.friese@gmail.com>
2016-11-16 07:12:28 -0800

    Split table view extension methods into separate files
    [#134432939]

---
+++ Fleet/CoreExtensions/TableView/UITableView+SelectRow.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+public extension UITableView {
+    /**
+     Mimic a user tap on the cell at the given index path in the table view.
+     Unlike UITableView's selectRow(at:animated:scrollPosition:) method, this
+     method fires the table view's hooked-in delegate call backs and sends a
+     notification.
+
+     - parameters:
+     - at: The index path to attempt to tap
+
+     - returns:
+     Nil if selection is successful, otherwise a FleetError if there is no cell at the given index path
+     or if the table view does not allow selection ('allowsSelection' == false)
+     */
+    public func selectRow(at indexPath: IndexPath) -> FleetError? {
+        guard allowsSelection else {
+            return FleetError(message: "Attempted to select row on table view with 'allowsSelection' == false")
+        }
+
+        let sectionCount = self.numberOfSections
+        if indexPath.section >= sectionCount {
+            return FleetError(message: "Invalid index path: Table view has no section \(indexPath.section) (section count in table view == \(sectionCount))")
+        }
+
+        let rowCount = self.numberOfRows(inSection: indexPath.section)
+        if indexPath.row >= rowCount {
+            return FleetError(message: "Invalid index path: Section \(indexPath.section) does not have row \(indexPath.row) (row count in section \(indexPath.section) == \(rowCount))")
+        }
+
+        if let selectedRowIndexPath = self.indexPathForSelectedRow {
+            let indexPathToDeselectOptional = self.delegate!.tableView!(self, willDeselectRowAt: selectedRowIndexPath)
+            if let indexPathToDeselect = indexPathToDeselectOptional {
+                self.deselectRow(at: indexPathToDeselect, animated: false)
+                NotificationCenter.default.post(name: NSNotification.Name.UITableViewSelectionDidChange, object: nil)
+                self.delegate?.tableView!(self, didDeselectRowAt: indexPathToDeselect)
+            }
+        }
+
+        let indexPathToSelectOptional = self.delegate!.tableView!(self, willSelectRowAt: indexPath)
+        guard let indexPathToSelect = indexPathToSelectOptional else {
+            return nil
+        }
+
+        self.selectRow(at: indexPath, animated: false, scrollPosition: .none)
+        NotificationCenter.default.post(name: NSNotification.Name.UITableViewSelectionDidChange, object: nil)
+
+        self.delegate!.tableView!(self, didSelectRowAt: indexPathToSelect)
+        return nil
+    }
+}
--- FleetTests/CoreExtensions/TableView/UITableView+SelectRowSpec.swift
+++ FleetTests/CoreExtensions/TableView/UITableView+SelectRowSpec.swift
@@ -4,7 +4,7 @@ import Nimble
 
 @testable import FleetTestApp
 
-class UITableView_FleetSpec: XCTestCase {
+class UITableView_SelectRowSpec: XCTestCase {
     func test_selectRow_whenACellExistsAtThatIndexPath_selectsTheCell() {
         let storyboard = UIStoryboard(name: "Birds", bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: "BirdsViewController") as! BirdsViewController
@@ -175,81 +175,5 @@ class UITableView_FleetSpec: XCTestCase {
         let error = viewController.birdsTableView?.selectRow(at: IndexPath(row: 0, section: 0))
         expect(error).toNot(beNil())
         expect(String(describing: error!)).to(equal("Fleet error: Attempted to select row on table view with 'allowsSelection' == false"))
-    }
-
-    func test_selectCellAction_whenTheActionExists_performsTheAction() {
-        let storyboard = UIStoryboard(name: "Birds", bundle: nil)
-        let viewController = storyboard.instantiateViewController(withIdentifier: "BirdsViewController") as! BirdsViewController
-        let _ = viewController.view
-
-        let error = viewController.birdsTableView?.selectCellAction(withTitle: "Two", at: IndexPath(row: 10, section: 0))
-        expect(error).to(beNil())
-        expect((viewController.presentedViewController as? UIAlertController)).toEventuallyNot(beNil())
-        expect((viewController.presentedViewController as? UIAlertController)?.message).toEventually(equal("Two tapped at row 10"))
-    }
-
-    func test_selectCellAction_whenTheActionExists_callsWillBeginEditingOnDelegate() {
-        let storyboard = UIStoryboard(name: "Birds", bundle: nil)
-        let viewController = storyboard.instantiateViewController(withIdentifier: "BirdsViewController") as! BirdsViewController
-        let _ = viewController.view
-
-        let _ = viewController.birdsTableView?.selectCellAction(withTitle: "Two", at: IndexPath(row: 10, section: 0))
-        expect(viewController.willBeginEditingRowCallArgs.count).to(equal(1))
-        expect(viewController.willBeginEditingRowCallArgs.first).to(equal(IndexPath(row: 10, section: 0)))
-    }
-
-    func test_selectCellAction_whenTheActionExists_callsDidEndEditingOnDelegate() {
-        let storyboard = UIStoryboard(name: "Birds", bundle: nil)
-        let viewController = storyboard.instantiateViewController(withIdentifier: "BirdsViewController") as! BirdsViewController
-        let _ = viewController.view
-
-        let _ = viewController.birdsTableView?.selectCellAction(withTitle: "Two", at: IndexPath(row: 10, section: 0))
-        expect(viewController.didEndEditingRowCallArgs.count).to(equal(1))
-        expect(viewController.didEndEditingRowCallArgs.first).to(equal(IndexPath(row: 10, section: 0)))
-    }
-
-    func test_selectCellAction_whenTheActionDoesNotExist_returnsAnError() {
-        let storyboard = UIStoryboard(name: "Birds", bundle: nil)
-        let viewController = storyboard.instantiateViewController(withIdentifier: "BirdsViewController") as! BirdsViewController
-        let _ = viewController.view
-
-        let error = viewController.birdsTableView?.selectCellAction(withTitle: "Four", at: IndexPath(row: 10, section: 0))
-        expect(String(describing: error!)).to(equal("Fleet error: Could not find edit action with title 'Four' at row 10 in section 0"))
-    }
-
-    func test_selectCellAction_whenNoCellExistsAtThatIndexPath_whenInvalidSectionInIndexPath_returnsAnError() {
-        let storyboard = UIStoryboard(name: "Birds", bundle: nil)
-        let viewController = storyboard.instantiateViewController(withIdentifier: "BirdsViewController") as! BirdsViewController
-        let _ = viewController.view
-        let error = viewController.birdsTableView?.selectCellAction(withTitle: "One", at: IndexPath(row: 1, section: 1))
-
-        expect(error).toNot(beNil())
-        expect(String(describing: error!)).to(equal("Fleet error: Invalid index path: Table view has no section 1 (section count in table view == 1)"))
-    }
-
-    func test_selectCellAction_whenNoCellExistsAtThatIndexPath_whenInvalidRowInIndexPath_returnsAnError() {
-        let storyboard = UIStoryboard(name: "Birds", bundle: nil)
-        let viewController = storyboard.instantiateViewController(withIdentifier: "BirdsViewController") as! BirdsViewController
-        let _ = viewController.view
-        let error = viewController.birdsTableView?.selectCellAction(withTitle: "One", at: IndexPath(row: 100, section: 0))
-
-        expect(error).toNot(beNil())
-        expect(String(describing: error!)).to(equal("Fleet error: Invalid index path: Section 0 does not have row 100 (row count in section 0 == 21)"))
-    }
-
-    func test_selectCellAction_whenDataSourceSaysThatIndexPathCannotBeEdited_returnsAnError() {
-        let storyboard = UIStoryboard(name: "Birds", bundle: nil)
-        let viewController = storyboard.instantiateViewController(withIdentifier: "BirdsViewController") as! BirdsViewController
-        let _ = viewController.view
-
-        // The fourth row in the table view is not editable
-        let errorOpt = viewController.birdsTableView?.selectCellAction(withTitle: "One", at: IndexPath(row: 3, section: 0))
-
-        guard let error = errorOpt else {
-            fail("Failed to return any FleetError")
-            return
-        }
-
-        expect(String(describing: error)).to(equal("Fleet error: Editing of row 3 in section 0 is not allowed by the table view's data source"))
     }
 }
