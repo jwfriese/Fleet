0767a6ffe6100f74cdda9256bb3e8157789a305f
Jared Friese <jared.friese@gmail.com>
2016-11-16 06:49:18 -0800

    Add code documentation for UITableView.selectCellAction(withTitle:at:)
    [#134432939]

--- Fleet/CoreExtensions/UITableView+Fleet.swift
+++ Fleet/CoreExtensions/UITableView+Fleet.swift
@@ -50,6 +50,19 @@ public extension UITableView {
         return nil
     }
 
+    /**
+    Mimic a user taking an edit action on the cell at the given index path in
+    the table view.
+
+    - parameters:
+        - title: The title of the edit action to take
+        - at: The index path at which to attempt to take this action
+
+    - returns:
+    Nil if edit action was taken successfully, otherwise a FleetError is there is no cell at
+    the given index path, if the data source does not allow editing of the given index path,
+    or if there is no edit action with the given title on the cell.
+    */
     func selectCellAction(withTitle title: String, at indexPath: IndexPath) -> FleetError? {
         let sectionCount = self.numberOfSections
         if indexPath.section >= sectionCount {
