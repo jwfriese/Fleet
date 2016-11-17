eb6d65b26e39b35c1e619c6ee09459c100407f4b
Jared Friese <jared.friese@gmail.com>
2016-11-17 06:09:50 -0800

    Expand testing of Fleet's behavior with respect to storyboard segues
    [#134511203]

--- FleetTestApp/Storyboards/Puppies/PuppyListViewController.swift
+++ FleetTestApp/Storyboards/Puppies/PuppyListViewController.swift
@@ -7,6 +7,13 @@ class PuppyListViewController: UIViewController {
     @IBOutlet weak var frenchBulldogButton: UIButton?
     @IBOutlet weak var basenjiButton: UIButton?
 
+    private var ShowCorgiSegueIdentifier: String { get { return "ShowCorgi" } }
+
+    var segueDestinationViewDidLoadCallCount: Int = 0
+    var segueDestinationViewController: UIViewController?
+
+    var corgiViewControllerImageViewIBOutletValue: UIImageView?
+
     @IBAction func showPuppiesList() {
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(1.5 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)) {
             self.revealPuppies()
@@ -18,5 +25,15 @@ class PuppyListViewController: UIViewController {
         malteseButton?.isHidden = false
         frenchBulldogButton?.isHidden = false
         basenjiButton?.isHidden = false
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == ShowCorgiSegueIdentifier {
+            if let corgiViewController = segue.destination as? CorgiViewController {
+                segueDestinationViewDidLoadCallCount = corgiViewController.viewDidLoadCallCount
+                segueDestinationViewController = segue.destination
+                corgiViewControllerImageViewIBOutletValue = corgiViewController.corgiImageView
+            }
+        }
     }
 }
---
+++ FleetTests/CoreExtensions/Storyboard/SegueDetailSpec.swift
@@ -0,0 +1,60 @@
+import XCTest
+import Fleet
+import Nimble
+@testable import FleetTestApp
+
+class SegueDetailSpec: XCTestCase {
+    func test_bindingViewControllersInSegues_doesNotCallViewDidLoadBeforePrepareForSegue() {
+        let storyboard = UIStoryboard(name: "PuppyStoryboard", bundle: nil)
+        let corgiViewController = CorgiViewController()
+        try! storyboard.bind(viewController: corgiViewController, toIdentifier: "CorgiViewController")
+
+        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
+        Fleet.setApplicationWindowRootViewController(navigationController)
+
+        guard let puppyViewController = navigationController.topViewController as? PuppyListViewController else {
+            fail("Failed to properly set up controller for segue detail test")
+            return
+        }
+
+        puppyViewController.performSegue(withIdentifier: "ShowCorgi", sender: nil)
+
+        expect(puppyViewController.segueDestinationViewDidLoadCallCount).to(equal(0))
+    }
+
+    func test_bindingViewControllersInSegues_makesTheBoundViewControllerAvailableAsDestinationViewControllerInTheSegue() {
+        let storyboard = UIStoryboard(name: "PuppyStoryboard", bundle: nil)
+        let corgiViewController = CorgiViewController()
+        try! storyboard.bind(viewController: corgiViewController, toIdentifier: "CorgiViewController")
+
+        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
+        Fleet.setApplicationWindowRootViewController(navigationController)
+
+        guard let puppyViewController = navigationController.topViewController as? PuppyListViewController else {
+            fail("Failed to properly set up controller for segue detail test")
+            return
+        }
+
+        puppyViewController.performSegue(withIdentifier: "ShowCorgi", sender: nil)
+
+        expect(puppyViewController.segueDestinationViewController).to(beIdenticalTo(corgiViewController))
+    }
+
+    func test_verifyNoIBOutletsPopulatedBeforePrepareForSegueWhenBindingViewControllersInSegues() {
+        let storyboard = UIStoryboard(name: "PuppyStoryboard", bundle: nil)
+        let corgiViewController = CorgiViewController()
+        try! storyboard.bind(viewController: corgiViewController, toIdentifier: "CorgiViewController")
+
+        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
+        Fleet.setApplicationWindowRootViewController(navigationController)
+
+        guard let puppyViewController = navigationController.topViewController as? PuppyListViewController else {
+            fail("Failed to properly set up controller for segue detail test")
+            return
+        }
+
+        puppyViewController.performSegue(withIdentifier: "ShowCorgi", sender: nil)
+
+        expect(puppyViewController.corgiViewControllerImageViewIBOutletValue).to(beNil())
+    }
+}
