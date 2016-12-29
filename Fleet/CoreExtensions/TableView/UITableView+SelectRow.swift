b3c6cddd4d4e53877e2b401007d7a734f6698597
Jared Friese <jared.friese@gmail.com>
2016-12-29 05:44:36 -0800

    Do not abort table view cell selection when optional UITableViewDelegate methods not implemented
    [#136777549]

--- Fleet/CoreExtensions/TableView/UITableView+SelectRow.swift
+++ Fleet/CoreExtensions/TableView/UITableView+SelectRow.swift
@@ -6,10 +6,10 @@ public extension UITableView {
      Unlike UITableView's selectRow(at:animated:scrollPosition:) method, this
      method fires the table view's hooked-in delegate call backs and sends a
      notification.
-
+     
      - parameters:
      - at: The index path to attempt to tap
-
+     
      - returns:
      Nil if selection is successful, otherwise a FleetError if there is no cell at the given index path
      or if the table view does not allow selection ('allowsSelection' == false)
@@ -18,35 +18,44 @@ public extension UITableView {
         guard allowsSelection else {
             return FleetError(message: "Attempted to select row on table view with 'allowsSelection' == false")
         }
-
+        
         let sectionCount = self.numberOfSections
         if indexPath.section >= sectionCount {
             return FleetError(message: "Invalid index path: Table view has no section \(indexPath.section) (section count in table view == \(sectionCount))")
         }
-
+        
         let rowCount = self.numberOfRows(inSection: indexPath.section)
         if indexPath.row >= rowCount {
             return FleetError(message: "Invalid index path: Section \(indexPath.section) does not have row \(indexPath.row) (row count in section \(indexPath.section) == \(rowCount))")
         }
-
-        if let selectedRowIndexPath = self.indexPathForSelectedRow {
-            let indexPathToDeselectOptional = self.delegate!.tableView!(self, willDeselectRowAt: selectedRowIndexPath)
-            if let indexPathToDeselect = indexPathToDeselectOptional {
-                self.deselectRow(at: indexPathToDeselect, animated: false)
-                NotificationCenter.default.post(name: NSNotification.Name.UITableViewSelectionDidChange, object: nil)
-                self.delegate?.tableView!(self, didDeselectRowAt: indexPathToDeselect)
+        
+        let doesDelegateImplementWillDeselect = self.delegate!.responds(to: #selector(UITableViewDelegate.tableView(_:willDeselectRowAt:)))
+        if doesDelegateImplementWillDeselect {
+            if let selectedRowIndexPath = self.indexPathForSelectedRow {
+                let indexPathToDeselectOptional = self.delegate!.tableView!(self, willDeselectRowAt: selectedRowIndexPath)
+                if let indexPathToDeselect = indexPathToDeselectOptional {
+                    self.deselectRow(at: indexPathToDeselect, animated: false)
+                    NotificationCenter.default.post(name: NSNotification.Name.UITableViewSelectionDidChange, object: nil)
+                    self.delegate?.tableView?(self, didDeselectRowAt: indexPathToDeselect)
+                }
             }
         }
-
-        let indexPathToSelectOptional = self.delegate!.tableView!(self, willSelectRowAt: indexPath)
-        guard let indexPathToSelect = indexPathToSelectOptional else {
-            return nil
+        
+        var indexPathToSelect = indexPath
+        let doesDelegateImplementWillSelect = self.delegate!.responds(to: #selector(UITableViewDelegate.tableView(_:willSelectRowAt:)))
+        if doesDelegateImplementWillSelect {
+            let indexPathToSelectOptional = self.delegate!.tableView!(self, willSelectRowAt: indexPath)
+            guard let unwrappedIndexPathToSelect = indexPathToSelectOptional else {
+                return nil
+            }
+            
+            indexPathToSelect = unwrappedIndexPathToSelect
         }
-
+        
         self.selectRow(at: indexPath, animated: false, scrollPosition: .none)
         NotificationCenter.default.post(name: NSNotification.Name.UITableViewSelectionDidChange, object: nil)
-
-        self.delegate!.tableView!(self, didSelectRowAt: indexPathToSelect)
+        
+        self.delegate!.tableView?(self, didSelectRowAt: indexPathToSelect)
         return nil
     }
 }
---
+++ FleetTests/CoreExtensions/TableView/UITableView+SelectRowMinimalDelegateImplSpec.swift
@@ -0,0 +1,60 @@
+import XCTest
+import Fleet
+import Nimble
+
+@testable import FleetTestApp
+import UIKit
+
+fileprivate class MinimallyImplementedDelegate: NSObject {}
+extension MinimallyImplementedDelegate: UITableViewDelegate {}
+extension MinimallyImplementedDelegate: UITableViewDataSource {
+    fileprivate func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    fileprivate func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+class UITableView_SelectRowMinimalDelegateImplSpec: XCTestCase {
+    func test_selectRow_whenTheDelegateDoesNotImplementWillSelect_stillSelectsRow() {
+        let storyboard = UIStoryboard(name: "Birds", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "BirdsViewController") as! BirdsViewController
+        let _ = viewController.view
+        
+        let delegate = MinimallyImplementedDelegate()
+        viewController.birdsTableView?.delegate = delegate
+        viewController.birdsTableView?.dataSource = delegate
+        
+        let _ = viewController.birdsTableView?.selectRow(at: IndexPath(row: 1, section: 0))
+        
+        guard let selectedIndexPath = viewController.birdsTableView?.indexPathForSelectedRow else {
+            fail("Failed to select row at index path (\(IndexPath(row: 1, section: 0)))")
+            return
+        }
+        
+        expect(selectedIndexPath).to(equal(IndexPath(row: 1, section: 0)))
+    }
+    
+    func test_selectRow_whenACellWasPreviouslySelected_whenTheDelegateDoesNotImplementWillDeselect_stillDeselectsThePreviouslySelectedCell() {
+        let storyboard = UIStoryboard(name: "Birds", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "BirdsViewController") as! BirdsViewController
+        let _ = viewController.view
+        
+        let delegate = MinimallyImplementedDelegate()
+        viewController.birdsTableView?.delegate = delegate
+        viewController.birdsTableView?.dataSource = delegate
+        
+        let _ = viewController.birdsTableView?.selectRow(at: IndexPath(row: 1, section: 0))
+        let _ = viewController.birdsTableView?.selectRow(at: IndexPath(row: 4, section: 0))
+        
+        guard let selectedIndexPaths = viewController.birdsTableView?.indexPathsForSelectedRows else {
+            fail("Failed to select any rows")
+            return
+        }
+        
+        expect(selectedIndexPaths.count).to(equal(1))
+        expect(selectedIndexPaths.first).to(equal(IndexPath(row: 4, section: 0)))
+    }
+}
