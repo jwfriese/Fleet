7fcea2460b967871d4eccabf6cbb5509ddcbbf17
Jared Friese <jared.friese@gmail.com>
2017-01-08 15:50:54 -0800

    Write test to ensure alerts call dismiss code on main thread
    [#127427599]

--- FleetTests/CoreExtensions/UIAlertController+FleetSpec.swift
+++ FleetTests/CoreExtensions/UIAlertController+FleetSpec.swift
@@ -48,4 +48,26 @@ class UIAlertController_FleetSpec: XCTestCase {
         alert.tapAlertAction(withTitle: "cancel")
         expect(didCallHandlerAfterDismissingAlert).toEventually(beTrue())
     }
+
+    func test_tapAlertActionWithTitle_callsUIKitMethodsOnMainThread() {
+        let rootViewController = UIViewController()
+        Fleet.setApplicationWindowRootViewController(rootViewController)
+
+        let alert = UIAlertController(title: "title", message: "message", preferredStyle: .alert)
+        var didCallOnMainThread = false
+        let actionHandler: (UIAlertAction) -> () = { _ in
+            if Fleet.getApplicationScreen()?.topmostViewController === rootViewController {
+                didCallOnMainThread = Thread.isMainThread
+            }
+        }
+
+        let action = UIAlertAction(title: "action", style: .default, handler: actionHandler)
+        alert.addAction(action)
+        rootViewController.present(alert, animated: false, completion: nil)
+
+        expect(Fleet.getApplicationScreen()?.topmostViewController).to(beIdenticalTo(alert))
+
+        alert.tapAlertAction(withTitle: "action")
+        expect(didCallOnMainThread).toEventually(beTrue())
+    }
 }
