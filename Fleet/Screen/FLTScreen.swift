693b80635b2bb37242d0d496bcbbbe0455ece5be
Jared Friese <jared.friese@gmail.com>
2017-01-22 11:52:06 -0800

    Add in-code documentation to `FLTScreen` interface
    [#137963565]

--- Fleet/Screen/FLTScreen.swift
+++ Fleet/Screen/FLTScreen.swift
@@ -1,6 +1,23 @@
 import UIKit
 
 public protocol FLTScreen {
+    /**
+     Gives access to the topmost view controller on the screen. It returns the `UIViewController`
+     visually at the very top of the `FLTScreen`'s view hierarchy.
+     */
     var topmostViewController: UIViewController? { get }
+
+    /**
+     Gives access to the topmost alert on the screen. It returns the `UIAlertController`
+     visually at the very top of the `FLTScreen`'s view hierarchy.
+
+     - note:
+     This method should perform in a functionally equivalent manner to:
+     ```
+     let screen: FLTScreen
+     // get a screen
+     let alertController = screen.topmostViewController as? UIAlertController
+     ```
+     */
     var presentedAlert: UIAlertController? { get }
 }
