ec0d661eda12ffb5f46302e3dc5ed8b7c52fa8c3
Jared Friese <jared.friese@gmail.com>
2017-01-22 10:37:24 -0800

    Add in-code documentation to `UISwitch.flip()`
    [#138108955]

--- Fleet/CoreExtensions/UISwitch+Fleet.swift
+++ Fleet/CoreExtensions/UISwitch+Fleet.swift
@@ -1,6 +1,12 @@
 import UIKit
 
 extension UISwitch {
+    /**
+     Toggles the switch value and fires the `.valueChanged` control event.
+
+     - returns:
+     A `FleetError` if the switch is hidden or disabled.
+    */
     public func flip() -> FleetError? {
         guard isEnabled else {
             return FleetError(message: "Cannot flip UISwitch: Control is not enabled")
