bde666d9dd138127987425341d7cf4d05a80a59e
Jared Friese <jared.friese@gmail.com>
2017-01-22 11:29:43 -0800

    Add in-code documentation for Fleet's static helpers
    [#137963565]

--- Fleet/Fleet.swift
+++ Fleet/Fleet.swift
@@ -1,6 +1,12 @@
 import UIKit
 
 public class Fleet {
+    /**
+     Provides a quick way of getting at the "main" application window in test.
+
+     - returns:
+     A `FLTScreen` instance wrapping the application's key window.
+     */
     public static func getApplicationScreen() -> FLTScreen? {
         guard let window = UIApplication.shared.keyWindow else {
             Logger.logWarning("Cannot get application screen: UIApplication not set up with a key window.")
@@ -10,10 +16,22 @@ public class Fleet {
         return Screen(forWindow: window)
     }
 
+    /**
+     Wraps the given window in an instance of `FLTScreen` and returns it.
+
+     - returns:
+     A `FLTScreen` instance wrapping the given `UIWindow` instance.
+     */
     public static func getScreen(forWindow window: UIWindow) -> FLTScreen {
         return Screen(forWindow: window)
     }
 
+    /**
+     Sets the given `UIViewController` as the main application window's root view controller.
+
+     - note:
+     This will kick off the given view controller's lifecycle.
+     */
     public static func setApplicationWindowRootViewController(_ viewController: UIViewController) {
         UIApplication.shared.keyWindow?.rootViewController = viewController
     }
