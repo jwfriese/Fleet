655999295a375e7f2c87d018e1b205aa9b7a795a
Jared Friese <jared.friese@gmail.com>
2017-10-10 09:05:49 -0700

    Print complete  error message when `swallowAnyErrors` catches an error

--- Fleet/Error/FleetError.swift
+++ Fleet/Error/FleetError.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public class FleetError: NSException {
     init(_ definition: FleetErrorDefinition) {
-        super.init(name: definition.name, reason: definition.errorMessage, userInfo: nil)
+        super.init(name: definition.name, reason: definition.errorMessage, userInfo: ["description" : definition.errorMessage])
     }
 
     required public init?(coder aDecoder: NSCoder) { super.init(coder: aDecoder) }
